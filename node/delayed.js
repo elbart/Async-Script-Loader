ef87438838b8eac7db8b2de7008b0775469aac24
elbart <tim@elbart.com>
2010-07-20 19:15:24 +0200

    replaced default markup iframe with self injected iframe

--- node/delayed.js
+++ node/delayed.js
@@ -2,7 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
     setTimeout(function() {
         res.writeHead(200, {'Content-Type': 'text/javascript'});
-        res.end('alert("huhu pete")');
+        res.end('console.log("loading of potentially external script done...");');
     }, 20000)
 }).listen(8124, "127.0.0.1");
-//console.log('Server running at http://127.0.0.1:8124/');
