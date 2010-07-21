698daf23f60d9d11af63bc6dbe551b3ac6236bcf
Tim Eggert <tim@elbart.com>
2010-07-21 09:27:08 +0200

    changed timeout + binding

--- node/delayed.js
+++ node/delayed.js
@@ -3,5 +3,5 @@ http.createServer(function (req, res) {
     setTimeout(function() {
         res.writeHead(200, {'Content-Type': 'text/javascript'});
         res.end('console.log("loading of potentially external script done...");');
-    }, 20000)
-}).listen(8124, "127.0.0.1");
+    }, 5000)
+}).listen(8124, "coding-geek.de");
